9d032e33ae5db5af73c8c1cb3d5ce371adfe3c0b
Rishav Kumar <rishavkr000@gmail.com>
2025-06-25 19:44:38 +0530

    Modify profile update logic

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,15 +2,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../redux/userSlice";
+import { ToastContainer, toast } from "react-toastify";
 
 const Profile = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const [nameEdit, setNameEdit] = useState(false);
-  const [emailEdit, setEmailEdit] = useState(false);
+  const [isEditEnable, setIsEditEnable] = useState(false);
   const [firstName, setFirstName] = useState(user?.firstName || "");
   const [lastName, setLastName] = useState(user?.lastName || "");
   const [emailId, setEmailId] = useState(user?.emailId || "");
+  const [phoneNumber, setPhoneNumber] = useState(user?.phoneNumber || "");
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -18,15 +19,17 @@ const Profile = () => {
       setFirstName(user.firstName || "");
       setLastName(user.lastName || "");
       setEmailId(user.emailId || "");
+      setPhoneNumber(user.phoneNumber || "");
     }
   }, [user]);
 
-  const handleNameEdit = () => {
-    setNameEdit(!nameEdit);
+  const handleEdit = () => {
+    setIsEditEnable(!isEditEnable);
   };
 
-  const handleEmailEdit = () => {
-    setEmailEdit(!emailEdit);
+  const isValidPhoneNumber = (phoneNumber) => {
+    const phoneRegex = /^[6-9]\d{9}$/;
+    return phoneRegex.test(phoneNumber);
   };
 
   const handleSave = async () => {
@@ -45,20 +48,31 @@ const Profile = () => {
         return;
       }
 
+      if (phoneNumber && phoneNumber.length != 10) {
+        setError("Phone number must be 10 digits");
+        return;
+      }
+
+      if (!isValidPhoneNumber(phoneNumber)) {
+        setError("Please enter a valid 10-digit Indian phone number.");
+        return;
+      }
+
       const res = await axios.patch(
         import.meta.env.VITE_BACKEND_URL + "/profile/update",
         {
           firstName,
           lastName,
-          emailId,
+          phoneNumber,
         },
         { withCredentials: true }
       );
-      setNameEdit(false);
-      setEmailEdit(false);
+      setIsEditEnable(false);
+      toast("Data updated successfully");
       dispatch(addUser(res.data.data));
     } catch (err) {
       setError(err.response.data.message);
+      toast(err.response.data.message);
     }
   };
 
@@ -68,54 +82,48 @@ const Profile = () => {
         <div className="card-body flex flex-row ">
           <h2 className="card-title">Personal Information</h2>
           <h2
-            onClick={handleNameEdit}
+            onClick={handleEdit}
             className="mx-5 mt-1 text-blue-600 cursor-pointer"
           >
-            {!nameEdit ? "Edit" : "Cancel"}
+            {!isEditEnable ? "Edit" : "Cancel"}
           </h2>
         </div>
         <div className="mx-6">
           <input
             type="text"
-            disabled={!nameEdit}
+            disabled={!isEditEnable}
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             className="border mr-4 p-2"
           />
           <input
             type="text"
-            disabled={!nameEdit}
+            disabled={!isEditEnable}
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             className="border p-2"
           />
-          {nameEdit && (
-            <button
-              onClick={handleSave}
-              className="ml-4 bg-primary px-9 py-2 rounded-xs cursor-pointer"
-            >
-              Save
-            </button>
-          )}
         </div>
         <div className="card-body flex flex-row ">
           <h2 className="card-title">Email Address</h2>
-          <h2
-            onClick={handleEmailEdit}
-            className="mx-5 mt-1 text-blue-600 cursor-pointer"
-          >
-            {!emailEdit ? "Edit" : "Cancel"}
-          </h2>
+          <h2 className="card-title mx-23">Phone Number</h2>
         </div>
         <div className="ml-6">
           <input
             type="text"
-            disabled={!emailEdit}
+            disabled
             value={emailId}
             onChange={(e) => setEmailId(e.target.value)}
             className="border mr-4 p-2"
           />
-          {emailEdit && (
+          <input
+            type="text"
+            disabled={!isEditEnable}
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            className="border mr-4 p-2"
+          />
+          {isEditEnable && (
             <button
               onClick={handleSave}
               className="bg-primary px-8 py-2 rounded-xs cursor-pointer"
@@ -126,12 +134,11 @@ const Profile = () => {
         </div>
         <div className="mx-6 pt-4 text-red-500">{error}</div>
         <div className="mx-6 my-10">
-          <h1 className="mb-5 text-blue-500 cursor-pointer">
-            Deactivate Account
-          </h1>
-          <h1 className="text-red-500 cursor-pointer">Delete Account</h1>
+          <h1 className="mb-5 text-blue-500 cursor-pointer">Change Password</h1>
+          <h1 className="text-red-500 cursor-pointer">Deactivate Account</h1>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
